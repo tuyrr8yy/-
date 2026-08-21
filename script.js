5284b67785147b3a8ae5b3f5b46767732dc7dfd6
tuyrr8yy <abinshee22@gmail.com>
2026-08-21 13:32:48 +0300

    Update API endpoint for fetching doctors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // جلب قائمة الأطباء والمواعيد
 async function fetchDoctors() {
     try {
-        const response = await fetch('https://api.example.com/doctors');
+        const response = await fetch('https://mock.apidog.com/m1/1363688-1367672-default/doctors');
         const doctors = await response.json();
         console.log('الأطباء المتاحون:', doctors);
     } catch (error) {
